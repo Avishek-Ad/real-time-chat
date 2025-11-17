ef0071ca575b2f37b526dc3ed7aa0513c502f5bb
Avishek-Ad <adhikariavishek03@gmail.com>
2025-11-18 02:40:43 +0545

    fixed for console email as railway doesnot allow smtp

--- a_core/settings.py
+++ a_core/settings.py
@@ -204,14 +204,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_ADDRESS') 
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = f"Real-Time Chat {env('EMAIL_ADDRESS')}"
-ACCOUNT_EMAIL_SUBJECT_PREFIX = '' 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = env('EMAIL_ADDRESS') 
+# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') 
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = f"Real-Time Chat {env('EMAIL_ADDRESS')}"
+# ACCOUNT_EMAIL_SUBJECT_PREFIX = '' 
 
 
 ACCOUNT_LOGIN_METHODS = {'email', 'username'}
