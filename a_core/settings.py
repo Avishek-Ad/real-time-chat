16be9e07a4e00cc26ed426be3721fcd24b8102ee
Avishek-Ad <adhikariavishek03@gmail.com>
2025-11-18 01:26:51 +0545

    added allowed host

--- a_core/settings.py
+++ a_core/settings.py
@@ -35,10 +35,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://real-time-chat-production-8147.up.railway.app/']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
-
+CSRF_TRUSTED_ORIGINS = [ 'https://real-time-chat-production-8147.up.railway.app/' ]
 
 # Application definition
 
